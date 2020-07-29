f89f2e91752b719ab7e1181fa857e083703c30c3
benwedeen <66267637+benwedeen@users.noreply.github.com>
2020-07-28 17:22:17 -0700

    update shell

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-##
 pip3  install -r requirements.txt
-<<<<<<< HEAD
 python3 -m pytest  Tests\test_easy.py -v -s --alluredir=allure-results
-=======
-python3 -m pytest tests_easy.py -v -s --alluredir=allure-results
->>>>>>> origin/master
