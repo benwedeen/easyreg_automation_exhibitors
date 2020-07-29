5c63daeca09784bbfc69044a6d562191299f851a
benwedeen <66267637+benwedeen@users.noreply.github.com>
2020-07-28 17:08:55 -0700

    Update start.sh
    remove Tests\

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip3  install -r requirements.txt
-python3 -m pytest  Tests\tests_easy.py -v -s --alluredir=allure-results
+python3 -m pytest tests_easy.py -v -s --alluredir=allure-results
